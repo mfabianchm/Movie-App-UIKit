4c97cdc09bcbf29ccef242aa4d6b3a3a1fa6d2a6
mfabianchm <mfchongm@hotmail.com>
2024-05-22 10:50:08 -0600

    Initial Commit

---
+++ Movie-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie-App
+//
+//  Created by Marcos Fabian Chong Megchun on 22/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
