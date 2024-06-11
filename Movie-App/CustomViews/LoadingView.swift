7084a6926a6886a77d59feda39f9d7387a8eab5f
Marcos Chong <88202552+mfabianchm@users.noreply.github.com>
2024-06-11 15:53:20 -0600

    Infinite Caroussel Implementation added, loadingView added

---
+++ Movie-App/CustomViews/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  Movie-App
+//
+//  Created by Marcos Chong on 11/06/24.
+//
+
+import UIKit
+
+class LoadingView: UIActivityIndicatorView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
