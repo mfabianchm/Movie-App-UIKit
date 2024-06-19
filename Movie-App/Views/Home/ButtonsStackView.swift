e082d86650f35b346c5bb1cb760a9780493236e3
Marcos Chong <88202552+mfabianchm@users.noreply.github.com>
2024-06-18 21:30:20 -0600

    ButtonStackView created

---
+++ Movie-App/Views/Home/ButtonsStackView.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonsStackView.swift
+//  Movie-App
+//
+//  Created by Marcos Chong on 18/06/24.
+//
+
+import UIKit
+
+class ButtonsStackView: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
